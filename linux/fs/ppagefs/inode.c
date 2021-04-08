77ab25f29e454c5cc12433703ad56226f1f520d5
RohanKumarSachdeva <rohan231094@gmail.com>
2021-04-07 20:21:40 -0700

    added filesytetem initialization and registration functions

--- linux/fs/ppagefs/inode.c
+++ linux/fs/ppagefs/inode.c
@@ -1,6 +1,20 @@
 /*
  * linux/fs/ppagefs/inode.c
  * This is a psuedo file system that exports kernel information
- * about the process physical page usage information if currently 
- * running process
+ * about the process physical page usage of currently 
+ * running process.
  */
+
+#include<linux/fs.h>
+#include<linux/module.h>
+#include<linux/init.h>
+#include<linux/slab.h>
+#include<linux/magic.h>
+
+static struct file_system_type ppage_fs_type = {
+	.owner = 	THIS_MODULE,
+	.name = 	"ppagefs",
+	.mount = 	ppage_mount,
+	.kill = 	kill_litter_super,
+};
+
