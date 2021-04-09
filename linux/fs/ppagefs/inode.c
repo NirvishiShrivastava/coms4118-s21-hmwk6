1d3fe05011261e0988630a46b78c70829c7c8ab2
NirvishiShrivastava <77449166+NirvishiShrivastava@users.noreply.github.com>
2021-04-09 17:56:38 +0530

    Added ppage_mount and ppage_fill_super functions

--- linux/fs/ppagefs/inode.c
+++ linux/fs/ppagefs/inode.c
@@ -11,7 +11,26 @@
 #include<linux/slab.h>
 #include<linux/magic.h>
 
-/* TODO: create ppage_mount and ppage_fill_super functions */
+/* TODO: create ppage_create_dir and inode functions */
+static int ppage_fill_super(struct super_block *sb, void *data, int silent)
+{
+	static const struct tree_descr ppage_files[] = {{""}};
+	int err;
+	char dir_name[] = "ppagefs";
+	err = simple_fill_super(sb, PPAGEFS_MAGIC, ppage_files);
+	if(err)
+		goto fail;
+	ppage_create_dir(sb, sb->s_root, dir_name);
+fail:
+	return err;
+}
+
+static struct dentry *ppage_mount(struct file_system_type *fs_type,
+			int flags, const char *dev_name, 
+			void *data)
+{
+	return mount_single(fs_type, flags, data, ppage_fill_super);
+}
 
 static struct file_system_type ppage_fs_type = {
 	.owner = 	THIS_MODULE,
