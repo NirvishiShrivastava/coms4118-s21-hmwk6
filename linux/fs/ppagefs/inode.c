5d517dcd3112430883eb4a2ce35b5b05fa02ac46
RohanKumarSachdeva <rohan231094@gmail.com>
2021-04-09 18:02:50 +0530

    added ppage_create_dir and ppage_create_inode func

--- linux/fs/ppagefs/inode.c
+++ linux/fs/ppagefs/inode.c
@@ -10,8 +10,56 @@
 #include<linux/init.h>
 #include<linux/slab.h>
 #include<linux/magic.h>
+#include<linux/pagemap.h>
 
-/* TODO: create ppage_create_dir and inode functions */
+static struct inode *ppage_make_inode(struct super_block *sb,
+			       int mode)
+{
+	struct inode *inode;
+
+	inode = new_inode(sb);
+
+	if (!inode)
+		return NULL;
+
+	inode->i_ino = get_next_ino();
+	inode->i_mtime = inode->i_atime = inode->i_ctime = current_time(inode);
+	inode->i_mode = mode;
+	inode->i_blkbits = PAGE_SHIFT;
+	inode->i_blocks = 0;
+
+	return inode;
+};
+
+struct dentry *ppage_create_dir(struct super_block *sb,
+				struct dentry *dir, const char *name)
+{
+	struct dentry *dentry;
+	struct inode *inode;
+	struct qstr qname;
+
+	qname.name = name;
+	qname.len = strlen(name);
+	qname.hash = full_name_hash(dir, name, qname.len);
+
+	dentry = d_alloc(dir, &qname);
+	if (!dentry)
+		return NULL;
+
+	inode = ppage_make_inode(sb, S_IFDIR | 0755);
+	if (!inode) {
+		dput(dentry);
+		return NULL;
+	}
+
+	inode->i_op = &simple_dir_inode_operations;
+	inode->i_fop = &simple_dir_operations;
+
+	d_add(dentry, inode);
+	return dentry;
+
+};
+
 static int ppage_fill_super(struct super_block *sb, void *data, int silent)
 {
 	static const struct tree_descr ppage_files[] = {{""}};
