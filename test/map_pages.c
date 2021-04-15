ce89150cac14958c1153e263a7e492e414904e13
Nirvishi Shrivastava <ns3499@columbia.edu>
2021-04-15 09:45:10 -0700

    Corrected do_inspect_pages function

--- test/map_pages.c
+++ test/map_pages.c
@@ -5,51 +5,56 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <dirent.h>
 
 #define PAGE_SIZE 0x1000UL
 
 void do_inspect_pages(int pid)
 {
-    char subdir_name[30] = "/mnt/";
-    FILE *fp;
-    DIR *d;
-    char *token_pid;
-    char ch, s[2] = ".", pid_str[10];
-    struct dirent *dir;
-
-    sprintf(pid_str,"%d",pid);
+	FILE *fp;
+	DIR *d;
+	char ch, pid_str[50], d_path[260], *token_pid, subdir_name[100] = "/mnt/";
+	struct dirent *dir;
+	
+	sprintf(pid_str,"%d",pid);
+	
+	/* extract sub directory name from pid */
+	d = opendir("/mnt");
+	if (d) {
+		while ((dir = readdir(d)) != NULL) {
+			/* Exclude . and .. directories */
+			if(dir->d_type == DT_DIR && strcmp(dir->d_name,".")!=0 && strcmp(dir->d_name,"..")!=0) {
+				sprintf(d_path,"%s", dir->d_name);
+				token_pid = strtok(dir->d_name,".");
+				
+				if(strcmp(token_pid,pid_str) == 0) {
+					// Form subdir path
+					printf("\nPID matched with subdirectory : %s\n",d_path);
+					strcat(subdir_name,d_path);
+					break;
+				}
+			}
+		}
+		closedir(d);
+	}
+	
+	/* Read total */
+	strcat(subdir_name,"/total");
+	fp = fopen(subdir_name, "r");
+	printf("File opening - %s\n",subdir_name);
+	
+	if (fp == NULL) {
+		perror("Error while opening the file");
+		exit(EXIT_FAILURE);
+	}
+	
+	while((ch = fgetc(fp)) != EOF)
+		printf("%c", ch);
+	
+	fclose(fp);
     
-    /* extract sub directory name from pid */
-    d = opendir("/mnt");
-    if (d) {
-        while ((dir = readdir(d)) != NULL) {
-            printf("%s\n", dir->d_name);
-            token_pid = strtok(dir->d_name, s);
-            
-            if(strcmp(token_pid,pid_str) == 0) {
-                /* Form subdir path */
-                strcat(subdir_name,dir->d_name);
-                break;
-            }
-        }
-        closedir(d);
-    }
-        
-    /* Read total pages*/
-    fp = fopen(strcat(subdir_name,"/total"), "r");
-    
-    if (fp == NULL) {
-        perror("Error while opening the file.\n");
-        exit(EXIT_FAILURE);
-    }
-    
-    while((ch = fgetc(fp)) != EOF)
-        printf("%c\n", ch);
-    
-    fclose(fp);
-    
-    /* Read Zero */
-    /*fp = fopen(strcat(subdir_name,"/zero"), "r");
+    /* Read Zero 
+    fp = fopen(strcat(subdir_name,"/zero"), "r");
     
     if (fp == NULL) {
         perror("Error while opening the file.\n");
@@ -63,28 +68,26 @@ void do_inspect_pages(int pid)
     
 }
 
-
 int main(void)
 {
-    int pid, p_pid;
-    void *addr;
-    unsigned long size = PAGE_SIZE * 10;
-    
-    p_pid = fork();
-    if (p_pid == 0) {
-        pid = getpid();
+	int pid, p_pid;
+	void *addr;
+	unsigned long size = PAGE_SIZE * 10;
+	
+	p_pid = fork();
+	
+	if (p_pid == 0) {
+		pid = getpid();
+		addr = malloc(size);
+		
+		if (!addr)
+			return -ENOMEM;
+		
+		// Allocating zero pages
+		memset(addr, 'a', size);
+		do_inspect_pages(pid);
         
-        // TODO: Check if we can malloc or need to mmap
-        addr = malloc(size);
-        if (!addr)
-            return -ENOMEM;
-        
-        // Allocating zero pages
-        memset(addr, 0, size);
-        
-        do_inspect_pages(pid);
-        
-        printf("Press enter to transform few pages to non-zero pages:\n");
+/*        printf("Press enter to transform few pages to non-zero pages:\n");
         getchar();
         fflush(stdin);
         
@@ -102,13 +105,13 @@ int main(void)
         
         sleep(30);
         free(addr);
-        return 0;
-        
-    } else if (p_pid > 0) {
-            wait(NULL);
-    } else {
-            printf("fork() failed.\n");
-    }
-    return 0;
-    
+	return 0;
+  */      
+	} else if (p_pid > 0) {
+		wait(NULL);
+	} else {
+		printf("fork() failed.\n");
+	}
+	return 0;
+   
 }
