ee39691326851626bf2337e44005066f530c956f
Nirvishi Shrivastava <ns3499@columbia.edu>
2021-04-17 09:14:55 -0700

    Removed do_inspect_pages function

--- test/map_pages.c
+++ test/map_pages.c
@@ -9,113 +9,49 @@
 
 #define PAGE_SIZE 0x1000UL
 
-void do_inspect_pages(int pid)
-{
-	FILE *fp;
-	DIR *d;
-	char ch, pid_str[50], d_path[260], *token_pid, subdir_name[100] = "/mnt/";
-	struct dirent *dir;
-	
-	sprintf(pid_str,"%d",pid);
-	
-	/* extract sub directory name from pid */
-	d = opendir("/mnt");
-	if (d) {
-		while ((dir = readdir(d)) != NULL) {
-			/* Exclude . and .. directories */
-			if(dir->d_type == DT_DIR && strcmp(dir->d_name,".")!=0 && strcmp(dir->d_name,"..")!=0) {
-				sprintf(d_path,"%s", dir->d_name);
-				token_pid = strtok(dir->d_name,".");
-				
-				if(strcmp(token_pid,pid_str) == 0) {
-					// Form subdir path
-					printf("\nPID matched with subdirectory : %s\n",d_path);
-					strcat(subdir_name,d_path);
-					break;
-				}
-			}
-		}
-		closedir(d);
-	}
-
-	if(chdir(subdir_name)== -1)
-		perror("Error changing directory");
-
-	system("/bin/ls > /dev/null");
-	
-	/* Read total */
-	fp = fopen("total", "r");
-	printf("File opening - %s\n",subdir_name);
-	
-	if (fp == NULL) {
-		perror("Error while opening the file");
-		exit(EXIT_FAILURE);
-	}
-
-	while((ch = fgetc(fp)) != EOF)
-		printf("%c", ch);
-	
-	fclose(fp);
-    
-    /* Read Zero 
-    fp = fopen(strcat(subdir_name,"/zero"), "r");
-    
-    if (fp == NULL) {
-        perror("Error while opening the file.\n");
-        exit(EXIT_FAILURE);
-    }
-    
-    while((ch = fgetc(fp)) != EOF)
-        printf("%c\n", ch);
-    
-    fclose(fp); */
-    
-}
-
 int main(void)
 {
 	int pid, p_pid;
 	void *addr;
 	unsigned long size = PAGE_SIZE * 10;
-	
+	char inspect_pages[30];
+
 	p_pid = fork();
 	
 	if (p_pid == 0) {
 		pid = getpid();
+		
+		sprintf(inspect_pages,"./inspect_pages %d",pid);		
 		addr = malloc(size);
 		
 		if (!addr)
 			return -ENOMEM;
 		
-		// Allocating zero pages
-		memset(addr, 'a', size);
-		do_inspect_pages(pid);
-        
-/*        printf("Press enter to transform few pages to non-zero pages:\n");
-        getchar();
-        fflush(stdin);
-        
-        memset(addr, 'a', PAGE_SIZE * 4);
-        
-        do_inspect_pages(pid);
-        
-        printf("Press enter to scrub pages back to zero pages:\n");
-        getchar();
-        fflush(stdin);
-        
-        memset(addr, 0, size);
-        
-        do_inspect_pages(pid);
-        
-        sleep(30);
-        free(addr);
-	return 0;
-  */      
+		/* Allocating zero pages */
+		memset(addr, 0, size);
+		system(inspect_pages);
+		
+		printf("\nPress enter to transform few pages to non-zero pages:");
+		getchar();
+		fflush(stdin);
+		
+		memset(addr, 'a', PAGE_SIZE * 4);
+		system(inspect_pages);		
+		
+		printf("\nPress enter to scrub pages back to zero pages:");
+		getchar();
+		fflush(stdin);
+		
+		memset(addr, 0, size);
+		system(inspect_pages);		
+				
+		free(addr);
+		return 0;
+
 	} else if (p_pid > 0) {
 		wait(NULL);
 	} else {
 		printf("fork() failed.\n");
 	}
-	return 0;
-   
+	return 0;   
 }
